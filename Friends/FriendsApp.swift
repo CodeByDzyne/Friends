b035877d4296932068d76cdb5e4d53c37ccb270a
CodeByDzyne <wdenley@dzynesolutions.com>
2023-01-22 13:51:05 +1100

    Initial Commit

---
+++ Friends/FriendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsApp.swift
+//  Friends
+//
+//  Created by Wealthy Waz on 22/1/2023.
+//
+
+import SwiftUI
+
+@main
+struct FriendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
